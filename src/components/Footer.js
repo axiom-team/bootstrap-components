14ed509ff01dca5dfaf09dd5ca247e30672f62f2
Timur Uzel <timurt.uzel@gmail.com>
2017-03-12 11:25:29 -0400

    more footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,9 @@
 import React from 'react'
 import githubLogo from '../../res/github.svg'
 import nodeLogo from '../../res/node.svg'
+import twitterLogo from '../../res/twitter.svg'
+import facebookLogo from '../../res/facebook.svg'
+import emailLogo from '../../res/email.svg'
 import { Grid, Icon } from 'bootstrap-components'
 import '../styles/footer.scss'
 
@@ -13,36 +16,38 @@ export const Footer = () => (
     <Grid>
       <row>
         <col
-          xs={6}
-          sm={3}
-          md={2}
+          xs={12}
+          sm={6}
+          md={4}
+        >
+          <a href="https://github.com/axiom-team/bootstrap-components">
+            GitHub
+            <img
+              src={githubLogo}
+              alt="GitHub logo"
+            />
+          </a>
 
-          children={(
-            <a href="https://github.com/axiom-team/bootstrap-components">
-              GitHub
-              <img
-                src={githubLogo}
-                alt="GitHub logo"
-              />
-            </a>
-          )}
-        />
+          <a href="https://npmjs.com/package/bootstrap-components">
+            npm
+            <img src={nodeLogo} alt="Node logo"/>
+          </a>
 
-        <col
-          xs={6}
-          sm={3}
-          md={2}
+          <a href="https://twitter.com/timurtu">
+            Twitter
+            <img src={twitterLogo} alt="Twitter logo"/>
+          </a>
 
-          children={(
-            <a href="https://npmjs.com/package/bootstrap-components">
-              npm
-              <img
-                src={nodeLogo}
-                alt="Node logo"
-              />
-            </a>
-          )}
-        />
+          <a href="https://www.facebook.com/reactbootstrap/">
+            Facebook
+            <img src={facebookLogo} alt="Facebook logo"/>
+          </a>
+
+          <a href="mailto:timurt.uzel@gmail.com">
+            Email
+            <img src={emailLogo} alt="Email me at timurt.uzel@gmail.com"/>
+          </a>
+        </col>
 
         <col
           xs={12}
